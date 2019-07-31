210a4c6440267515871cbe149aae707a2b4acd42
EspiDev <espidev@gmail.com>
2019-07-31 18:02:22 -0400

    Added sftp and ftp and made zips work

---
+++ src/main/java/dev/espi/ebackup/BackupUtil.java
@@ -0,0 +1,201 @@
+package dev.espi.ebackup;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.TransportException;
+import net.schmizz.sshj.userauth.UserAuthException;
+import net.schmizz.sshj.xfer.FileSystemFile;
+import org.apache.commons.net.ftp.FTPClient;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/*
+   Copyright 2019 EstiNet
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
+public class BackupUtil {
+
+    // delete old backups (when limit reached)
+    private static void checkMaxBackups() {
+        if (eBackup.getPlugin().maxBackups <= 0) return;
+
+        int backups = 0;
+        SortedMap<Long, File> m = new TreeMap<>(); // oldest files to newest
+
+        for (File f : eBackup.getPlugin().backupPath.listFiles()) {
+            if (f.getName().endsWith(".zip")) {
+                backups++;
+                m.put(f.lastModified(), f);
+            }
+        }
+
+        // delete old backups
+        while (backups-- >= eBackup.getPlugin().maxBackups) {
+            m.get(m.firstKey()).delete();
+            m.remove(m.firstKey());
+        }
+    }
+
+    // actually do the backup
+    // run async plz
+    public static void doBackup() {
+        List<File> tempIgnore = new ArrayList<>();
+        eBackup.getPlugin().getLogger().info("Starting backup...");
+        try {
+            // find plugin data to ignore
+            for (File f : new File("plugins").listFiles()) {
+                if ((!eBackup.getPlugin().backupPluginJars && f.getName().endsWith(".jar")) || (!eBackup.getPlugin().backupPluginConfs && f.isDirectory())) {
+                    tempIgnore.add(f);
+                    eBackup.getPlugin().ignoredFiles.add(f);
+                }
+            }
+
+            // delete old backups
+            checkMaxBackups();
+
+            // zip
+            String fileName = eBackup.getPlugin().backupFormat.replace("{DATE}", new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date()));
+            FileOutputStream fos = new FileOutputStream(eBackup.getPlugin().backupPath + "/" + fileName + ".zip");
+            ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+            // backup worlds first
+            for (World w : Bukkit.getWorlds()) {
+                File world = new File(w.getName());
+                // check if world is in ignored list
+                boolean skip = false;
+                for (File f : eBackup.getPlugin().ignoredFiles) {
+                    if (f.getAbsolutePath().equals(world.getAbsolutePath())) {
+                        skip = true;
+                        break;
+                    }
+                }
+                if (skip) continue;
+
+                w.save();
+                w.setAutoSave(false); // make sure autosave doesn't screw everything over
+                zipFile(world, world.getName(), zipOut);
+                w.setAutoSave(true);
+
+                // ignore in dfs
+                tempIgnore.add(world);
+                eBackup.getPlugin().ignoredFiles.add(world);
+            }
+
+            // dfs all other files
+            zipFile(new File(Paths.get(".").toAbsolutePath().normalize().toString()), "", zipOut);
+            zipOut.close();
+            fos.close();
+
+            // upload to ftp/sftp
+            if (eBackup.getPlugin().ftpEnable) {
+                if (eBackup.getPlugin().ftpType.equals("sftp")) {
+                    eBackup.getPlugin().getLogger().info("Uploading backup to SFTP server...");
+                    uploadSFTP(new File(eBackup.getPlugin().backupPath + "/" + fileName + ".zip"));
+                } else if (eBackup.getPlugin().ftpType.equals("ftp")) {
+                    eBackup.getPlugin().getLogger().info("Uploading backup to FTP server...");
+                    uploadFTP(new File(eBackup.getPlugin().backupPath + "/" + fileName + ".zip"));
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            for (World w : Bukkit.getWorlds()) {
+                w.setAutoSave(true);
+            }
+            // restore tempignore
+            for (File f : tempIgnore) {
+                eBackup.getPlugin().ignoredFiles.remove(f);
+            }
+        }
+        eBackup.getPlugin().getLogger().info("Backup complete!");
+    }
+
+    private static void uploadSFTP(File f) throws IOException {
+        SSHClient ssh = new SSHClient();
+        ssh.loadKnownHosts();
+        ssh.connect(eBackup.getPlugin().ftpHost, eBackup.getPlugin().ftpPort);
+        try {
+            ssh.authPassword(eBackup.getPlugin().ftpUser, eBackup.getPlugin().ftpPass);
+            try (SFTPClient sftp = ssh.newSFTPClient()) {
+                sftp.put(new FileSystemFile(f.getAbsolutePath()), eBackup.getPlugin().ftpPath);
+            }
+        } catch (UserAuthException | TransportException e) {
+            e.printStackTrace();
+        } finally {
+            ssh.disconnect();
+        }
+    }
+
+    private static void uploadFTP(File f) {
+        FTPClient ftpClient = new FTPClient();
+        try (FileInputStream fio = new FileInputStream(f)) {
+            ftpClient.connect(eBackup.getPlugin().ftpHost, eBackup.getPlugin().ftpPort);
+            ftpClient.login(eBackup.getPlugin().ftpUser, eBackup.getPlugin().ftpPass);
+            ftpClient.storeFile(f.getAbsolutePath(), fio);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ftpClient.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // recursively compress files and directories
+    private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+        if (fileToZip.isHidden() && !fileToZip.getPath().equals(".")) return;
+        for (File f : eBackup.getPlugin().ignoredFiles) { // return if it is ignored file
+            if (f.getAbsolutePath().equals(fileToZip.getAbsolutePath())) return;
+        }
+
+        if (fileToZip.isDirectory()) { // recursively search
+            if (fileName.endsWith("/")) {
+                zipOut.putNextEntry(new ZipEntry(fileName));
+                zipOut.closeEntry();
+            } else {
+                zipOut.putNextEntry(new ZipEntry(fileName + "/"));
+                zipOut.closeEntry();
+            }
+            File[] children = fileToZip.listFiles();
+            for (File childFile : children) {
+                zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
+            }
+            return;
+        }
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileName);
+        zipOut.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        fis.close();
+    }
+}
---
+++ src/main/java/dev/espi/ebackup/eBackup.java
@@ -0,0 +1,139 @@
+package dev.espi.ebackup;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+   Copyright 2019 EstiNet
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
+public class eBackup extends JavaPlugin implements CommandExecutor {
+
+    // config options
+
+    String crontask, backupFormat;
+    File backupPath;
+    int maxBackups;
+
+    String ftpType, ftpHost, ftpUser, ftpPass, ftpPath;
+    int ftpPort;
+    boolean ftpEnable;
+
+    boolean backupPluginJars, backupPluginConfs;
+    List<String> filesToIgnore;
+    List<File> ignoredFiles = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Initializing eBackup...");
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+
+        if (!getDataFolder().exists()) getDataFolder().mkdir();
+
+        try {
+            getConfig().load(getDataFolder() + "/config.yml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        // load config data
+        crontask = getConfig().getString("crontask");
+        backupFormat = getConfig().getString("backup-format");
+        backupPath = new File(getConfig().getString("backup-path"));
+        maxBackups = getConfig().getInt("max-backups");
+        ftpEnable = getConfig().getBoolean("ftp.enable");
+        ftpType = getConfig().getString("ftp.type");
+        ftpHost = getConfig().getString("ftp.host");
+        ftpPort = getConfig().getInt("ftp.port");
+        ftpUser = getConfig().getString("ftp.user");
+        ftpPass = getConfig().getString("ftp.pass");
+        ftpPath = getConfig().getString("ftp.path");
+        backupPluginJars = getConfig().getBoolean("backup.pluginjars");
+        backupPluginConfs = getConfig().getBoolean("backup.pluginconfs");
+        filesToIgnore = getConfig().getStringList("backup.ignore");
+        for (String s : filesToIgnore) {
+            ignoredFiles.add(new File(s));
+        }
+        this.getCommand("ebackup").setExecutor(this);
+
+        if (!backupPath.exists()) backupPath.mkdir(); // make sure backup location exists
+
+        getLogger().info("Plugin initialized!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabled eBackup!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.AQUA + "Do /ebackup help for help!");
+            return true;
+        }
+
+        switch (args[0]) {
+            case "help":
+                sender.sendMessage(ChatColor.GRAY + "" + ChatColor.STRIKETHROUGH + "=====" + ChatColor.RESET + ChatColor.DARK_AQUA + " eBackup Help " + ChatColor.RESET + ChatColor.GRAY + ChatColor.STRIKETHROUGH + "=====");
+                sender.sendMessage(ChatColor.AQUA + "> " + ChatColor.GRAY + "/ebackup backup - Starts a backup of the server.");
+                sender.sendMessage(ChatColor.AQUA + "> " + ChatColor.GRAY + "/ebackup list - Lists the backups in the folder.");
+                sender.sendMessage(ChatColor.AQUA + "> " + ChatColor.GRAY + "/ebackup stats - Shows disk space.");
+                break;
+            case "backup":
+                sender.sendMessage("Starting backup...");
+                Bukkit.getScheduler().runTaskAsynchronously(getPlugin(), () -> {
+                    BackupUtil.doBackup();
+                    sender.sendMessage("Finished!");
+                });
+                break;
+            case "list":
+                sender.sendMessage(ChatColor.AQUA + "Local Backups:");
+                for (File f : getPlugin().backupPath.listFiles()) {
+                    sender.sendMessage(ChatColor.GRAY + "- " + f.getName());
+                }
+                break;
+            case "stats":
+                sender.sendMessage(ChatColor.GRAY + "" + ChatColor.STRIKETHROUGH + "=====" + ChatColor.RESET + ChatColor.DARK_AQUA + " Disk Stats " + ChatColor.RESET + ChatColor.GRAY + ChatColor.STRIKETHROUGH + "=====");
+                sender.sendMessage(ChatColor.AQUA + "Total size: " + ChatColor.GRAY + (getPlugin().backupPath.getTotalSpace()/1024/1024/1024) + "GB");
+                sender.sendMessage(ChatColor.AQUA + "Space usable: " + ChatColor.GRAY + (getPlugin().backupPath.getUsableSpace()/1024/1024/1024) + "GB");
+                sender.sendMessage(ChatColor.AQUA + "Space free: " + ChatColor.GRAY + (getPlugin().backupPath.getFreeSpace()/1024/1024/1024) + "GB");
+                break;
+            default:
+                sender.sendMessage(ChatColor.AQUA + "Do /ebackup help for help!");
+                break;
+        }
+        return true;
+    }
+
+    public static eBackup getPlugin() {
+        return (eBackup) Bukkit.getPluginManager().getPlugin("eBackup");
+    }
+
+}
